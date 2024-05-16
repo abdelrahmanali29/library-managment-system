86dad102733e549a3e3a1f44fe8573851e9f4d2a
abdelrahmanali29 <140985149+abdelrahmanali29@users.noreply.github.com>
2024-05-16 18:13:12 +0300

    Update StudentRecordManager.cpp

--- project progamming/StudentRecordManager.cpp
+++ project progamming/StudentRecordManager.cpp
@@ -1,12 +1,16 @@
 #include "StudentRecordManager.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 StudentRecordManager::StudentRecordManager()
 {
-	studentName = " ", studentID = 0, bookID = 0, department = " ";
+	studentName = " ", studentID = 0, bookID = " ", department = " ";
 }
 void StudentRecordManager::issueRecord()
 {
-
+	cin.ignore();
+	ofstream booksfile("c:\\MSA Programming Application\\issued book.csv", ios::app);
+	booksfile << bookID << "," << studentID << "\n";
+	booksfile.close();
 }
