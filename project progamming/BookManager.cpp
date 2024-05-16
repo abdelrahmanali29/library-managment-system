cd3755ea2c9ac65465c39f6dd39b5c0fc5e110c4
abdelrahmanali29 <140985149+abdelrahmanali29@users.noreply.github.com>
2024-05-16 18:09:35 +0300

    Update BookManager.cpp

--- project progamming/BookManager.cpp
+++ project progamming/BookManager.cpp
@@ -13,8 +13,7 @@ void BookManager::addBook()
 	cout << "enter the data of a the book you want to search about in a specific department " << endl;
 	cout << "enter the book's department" << endl;
 	getline(cin, department);
-	if (department != "Mechanical" || department != "Computer" || department != "Architectrue" ||
-		department != "Civil" || department != "Electronics" || department != "Electrical");
+	if (department != "Mechanical" || department != "Computer" || department != "Architectrue" || department != "Civil" || department != "Electronics" || department != "Electrical");
 	{
 		cout << "the department you enter does not exist in the system" << endl;
 		cout << "enter the book's department" << endl;
@@ -35,6 +34,49 @@ void BookManager::addBook()
 void BookManager::deleteBook()
 {
 	cin.ignore();
+	string line, word;
+	bool isfound = false;
+	cout << "enter the data of a the book you want to delete in a specific department " << endl;
+	cout << "enter the book's department" << endl;
+	getline(cin, department);
+	if (department != "Mechanical" || department != "Computer" || department != "Architectrue" || department != "Civil" || department != "Electronics" || department != "Electrical");
+	{
+		cout << "the department you enter does not exist in the system" << endl;
+		cout << "enter the book's department" << endl;
+		getline(cin, department);
+	}
+	cout << "enter the book's ID, Book's title or Book's author" << endl;
+	getline(cin, word);
+	string filename = "c:\\MSA Programming Application\\" + department + ".csv";
+	ifstream file(filename);
+	ofstream Temp("temp.txt");
+	while (getline(file, line))
+	{
+		if (line.find(word) == string::npos)
+		{
+			Temp << line << endl;
+		}
+		else
+		{
+			isfound = true;
+		}
+	}
+	file.close();
+	Temp.close();
+	if (isfound)
+	{
+		remove(filename.c_str());
+		rename("temp.txt", filename.c_str());
+		cout << "the book " << word << " is deleted from the file " << endl;
+	}
+	else
+	{
+		remove("temp.txt");
+		cout << "book " << word << " is not found in the file." << endl;
+	}
+}
+void BookManager::editBook() 
+{
 string line, word;
 bool isfound = false;
 cout << "enter the data of a the book you want to delete in a specific department " << endl;
@@ -80,10 +122,18 @@ void BookManager::editBook()
 // not yet complete
 void BookManager::searchBook()
 {
+	cin.ignore();
 	string word,line;
 	cout << "enter the data of a the book you want to search about in a specific department " << endl;
 	cout << "enter the book's department" << endl;
 	getline(cin, department);
+	if (department != "Mechanical" || department != "Computer" || department != "Architectrue" ||
+		department != "Civil" || department != "Electronics" || department != "Electrical");
+	{
+		cout << "the department you enter does not exist in the system" << endl;
+		cout << "enter the book's department" << endl;
+		getline(cin, department);
+	}
 	cout << "enter the book's ID, Book's title or Book's author" << endl;
 	getline(cin, word);
 	string filename = "c:\\MSA Programming Application\\" + department + ".csv";
@@ -97,17 +147,19 @@ void BookManager::searchBook()
 
 	file.close();
 }
-void BookManager::issueBook()
-{
-
-}
-// not yet complete
 void BookManager::viewBooks() 
 {
 	string line;
 	cin.ignore();
 	cout << "enter the department you want to view the books of" << endl;
 	getline(cin, department);
+	if (department != "Mechanical" || department != "Computer" || department != "Architectrue" ||
+		department != "Civil" || department != "Electronics" || department != "Electrical");
+	{
+		cout << "the department you enter does not exist in the system" << endl;
+		cout << "enter the book's department" << endl;
+		getline(cin, department);
+	}
 	string filename = "c:\\MSA Programming Application\\" + department + ".csv";
 	ifstream file(filename);
 	while (getline(file, line))
