cde4f39496b9eddee2afb0c6a5a8bfe55ea7cc4c
abdelrahmanali29 <140985149+abdelrahmanali29@users.noreply.github.com>
2024-05-18 18:34:18 +0300

    Update InputManager.cpp

--- project progamming/InputManager.cpp
+++ project progamming/InputManager.cpp
@@ -9,8 +9,8 @@ string InputManager::getdata()
 	getline(cin, studentName);
 	cout << "please enter student Id" << endl;
 	cin >> studentID;
-	cout << "please enter book Id you want to issue" << endl;
 	cin.ignore();
+	cout << "please enter book Id you want to issue" << endl;
 	getline(cin, bookID);
 	cout << "please enter book's department " << endl;
 	getline(cin, department);
@@ -21,16 +21,9 @@ void InputManager::CheckId(string bookID)
 	string line;
 	string filename = "c:\\MSA Programming Application\\" + department + ".csv";
 	ifstream file(filename, ios::app);
-	if (department != "Mechanical" || department != "Computer" || department != "Architectrue" ||
-		department != "Civil" || department != "Electronics" || department != "Electrical");
-	{
-		cout << "the department you enter does not exist in the system" << endl;
-		cout << "enter the book's department" << endl;
-		getline(cin, department);
-	}
 	while (getline(file, line))
 	{
-		if (line.find(bookID) != string::npos) 
+		if (line.find(bookID) != string::npos)
 			cout << "the book is found in the department you entered";
 		else
 		{
