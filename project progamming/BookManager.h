4aa23a3ebe02680a0c545e4093de48ad07a87df0
abdelrahmanali29 <140985149+abdelrahmanali29@users.noreply.github.com>
2024-05-18 18:33:50 +0300

    Update BookManager.h

--- project progamming/BookManager.h
+++ project progamming/BookManager.h
@@ -6,16 +6,15 @@ using namespace std;
 class BookManager
 {
 private:
-	string title, author;
-	string department;
+	string title, author, department;
+
 	int BookId;
 public:
 	BookManager();
 	void addBook();
 	void deleteBook();
-	void editBook();
+	void editBook() {}
 	void searchBook();
 	void viewBooks();
 };
 #endif
-
